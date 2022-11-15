97db0cddf2d85fd47c16d4c105be720167beecaa
Liknesh <likneshrao@gmail.com>
2022-11-15 23:45:32 +0800

    Created pages for study material including for 4,5 and 6 years old

---
+++ resources/views/student/study_6yrs.blade.php
@@ -0,0 +1,107 @@
+@extends('layouts.layout')
+
+@section('content')
+
+  <div class="border border-primary" style="margin-top: 20px; background-color: #496b097e;" >
+    
+    <div class="row" style="margin-left:20px;margin-top: 20px;;margin-bottom: 20px;">
+        <ul class="nav nav-pills" >
+          <li class="nav-item " style="margin-right: 15px; margin-left: 15px;">
+            <a style="padding-top:20px; padding-bottom:20px" class="nav-link active" aria-current="page" href="{{url('4material')}}">4 Year Old</a>
+          </li>
+          <li class="nav-item" style="margin-right: 15px; margin-left: 15px;">
+              <a style="padding-top:20px; padding-bottom:20px" class="nav-link active" aria-current="page" href="{{url('5material')}}">5 Year Old</a>
+            </li>
+            <li class="nav-item" style="margin-right: 15px; margin-left: 15px;">
+              <a style="padding-top:20px; padding-bottom:20px" class="nav-link active" aria-current="page" href="{{url('6material')}}">6 Year Old</a>
+            </li>
+          </ul>
+</div>
+
+
+
+   
+  
+</div>
+<div class="border border-warning" style="margin-top: 20px;"></div>
+<div class="border border-primary" style="margin-top: 20px; background-color: #496b097e;">
+    <div class="container">
+        <div class="row" style="margin-top: 20px; margin-bottom: 20px;" >
+          <div class="col col-lg-4" >
+            <div class="card" style="width: 18rem; border-style: solid;">
+                <div class="card-header">
+                    <h5 class="card-title">Subject1</h5>
+                    
+                  </div>
+                <img class="card-img-top" src="{{asset('img/cat-2.jpg')}}" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+          </div>
+          <div class="col col-lg-4">
+            
+            <div class="card" style="width: 18rem; border-style: solid;">
+                <div class="card-header">
+                    <h5 class="card-title">Subject2</h5>
+                  </div>
+                <img class="card-img-top" src="{{asset('img/cat-2.jpg')}}" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+          </div>
+          <div class="col col-lg-4">
+            
+            <div class="card" style="width: 18rem; border-style: solid;">
+                <div class="card-header">
+                    <h5 class="card-title">Subject3</h5>
+                  </div>
+                <img class="card-img-top" src="{{asset('img/cat-2.jpg')}}" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class="row justify-content-md-center" style="margin-bottom: 20px; margin-tops: 20px">
+            <div class="col col-lg-4">
+                
+                <div class="card" style="width: 18rem; border-style: solid;">
+                    <div class="card-header">
+                        <h5 class="card-title">Subject4</h5>
+                      </div>
+                    <img class="card-img-top" src="{{asset('img/cat-2.jpg')}}" alt="Card image cap">
+                    <div class="card-body">
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+            
+            <div class="col col-lg-4">
+               
+                <div class="card" style="width: 18rem; border-style: solid;">
+                    <div class="card-header">
+                        <h5 class="card-title">Subject5</h5>
+                      </div>
+                    <img class="card-img-top" src="{{asset('img/cat-2.jpg')}}" alt="Card image cap">
+                    <div class="card-body">
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+          </div>
+      </div>
+      
+</div>
+
+
+</div>
+
+
+@endsection
